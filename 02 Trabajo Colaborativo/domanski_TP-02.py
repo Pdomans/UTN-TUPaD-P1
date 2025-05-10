655a639724c68446df1ab6f0540381a99d704adb
pdomans <pablodomanski90@gmail.com>
2025-05-10 11:36:11 -0300

    preguntas hechas

---
+++ 02 Trabajo Colaborativo/domanski_TP-02.py
@@ -0,0 +1,60 @@
+""" 
+• ¿Qué es GitHub? 
+es un sistema de control de versiones 
+
+• ¿Cómo crear un repositorio en GitHub? 
+se entra a la pagina https://github.com/ voy a la seccion resopitorios y  voy a new 
+
+• ¿Cómo cambiar a una rama en Git?
+ con el comando git checkout "rama"
+
+ • ¿Cómo fusionar ramas en Git? 
+ con el comando git marge  rama 
+
+ ¿Cómo crear un commit en Git? 
+
+ con el comando git commit -m " comentario" 
+
+ • ¿Cómo enviar un commit a GitHub? 
+ con git push 
+
+• ¿Qué es un repositorio remoto? 
+es una version de mi proyecto que esta en la nube 
+
+• ¿Cómo agregar un repositorio remoto a Git? 
+con el comando git remote add 
+con git remote add " y el nombre del repo en la nuve "
+
+• ¿Cómo empujar cambios a un repositorio remoto? 
+con git push 
+
+• ¿Cómo tirar de cambios de un repositorio remoto? 
+no entendi la pregunta 
+
+
+• ¿Qué es un fork de repositorio? 
+es cuando copio un repositorio de un tercero a mi cuenta 
+
+¿Cómo crear un fork de un repositorio? 
+desde la pagina de github busco el repositorio que quiero adiquitir y  una vez localizado hago click en fork 
+
+• ¿Qué es un repositorio privado en GitHub? 
+   te logueas en https://github.com vas a la solapa repositorios y despues de la descripcion podes elegir si el repositorio es publico o privado  
+
+
+
+• ¿Cómo invitar a alguien a un repositorio privado en GitHub?
+ lo agregas como un colaborador 
+   
+
+ • ¿Qué es un repositorio público en GitHub? 
+  es un proyecto subido a la nuve de github donde es de accedido libremente
+
+• ¿Cómo crear un repositorio público en GitHub? 
+te logueas en https://github.com vas a la solapa repositorios y despues de la descripcion podes elegir si el repositorio es publico o privado  
+
+
+• ¿Cómo compartir un repositorio público en GitHub?
+vas a github seleccionas el repositorio publico que quieres compartir  y copias el URL  del navegador y compartes el enlace 
+
+ """
